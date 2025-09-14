748ca8cf2e3a346ee2519c1bd3a80f278d8da6ef
minho jang <mjang6988@gmail.com>
2025-09-14 17:21:53 +0900

    refactor: remove unused imports

--- src/main/kotlin/io/miinhho/recomran/place/repository/PlaceEntity.kt
+++ src/main/kotlin/io/miinhho/recomran/place/repository/PlaceEntity.kt
@@ -1,18 +1,11 @@
 package io.miinhho.recomran.place.repository
 
-import io.miinhho.recomran.history.repository.PlaceHistoryEntity
 import io.miinhho.recomran.place.model.Place
-import io.miinhho.recomran.saved.repository.SavedPlaceEntity
-import io.miinhho.recomran.saved.repository.SavedPlacePlaces
-import jakarta.persistence.CascadeType
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
-import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
-import jakarta.persistence.ManyToMany
-import jakarta.persistence.OneToMany
 import jakarta.persistence.Table
 
 @Entity
